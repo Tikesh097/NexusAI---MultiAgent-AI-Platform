e9b4ebf568e6ef648bc94184bb68ba5ec2cb6ed3
Tikesh <aswaletinku@gmail.com>
2026-09-03 09:32:08 +0530

    firebase and gateway isue fixed

--- backend/services/auth/config/firebase.js
+++ backend/services/auth/config/firebase.js
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import { initializeApp, cert } from "firebase-admin";
 
 const serviceAccount = {
@@ -6,6 +9,20 @@ const serviceAccount = {
   privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
 };
 
+
+if (!serviceAccount.projectId) {
+  throw new Error("FIREBASE_PROJECT_ID is missing");
+}
+
+if (!serviceAccount.clientEmail) {
+  throw new Error("FIREBASE_CLIENT_EMAIL is missing");
+}
+
+if (!serviceAccount.privateKey) {
+  throw new Error("FIREBASE_PRIVATE_KEY is missing");
+}
+
 export const app = initializeApp({
   credential: cert(serviceAccount),
 });
+
